0c6909e13734d7b111115ea5c2b16b1fdc8e19fb
Alex Cho <alexsjcho@gmail.com>
2019-04-02 15:14:48 -0700

    added PropTypes Testing

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 /**
  * Functional react component for congratulatory message.
@@ -7,15 +8,26 @@ import React from "react";
  * @returns {JSX.Element} - Rendered component (or null if `success` prop is false).
  */
 export default props => {
-  if (props.success) {
-    return (
-      <div data-test="component-congrats">
-        <span data-test="congrats-message">
-          Congratulations! You guessed the word!
-        </span>
-      </div>
-    );
-  } else {
-    return <div data-test="component-congrats" />;
-  }
-};
+const Congrats = (props) => {
+    if (props.success) {
+        return (
+          <div data-test="component-congrats">
+            <span data-test="congrats-message">
+              Congratulations! You guessed the word!
+            </span>
+          </div>
+        );
+      } else {
+        return <div data-test="component-congrats" />;
+      }
+    };
+}
+
+
+
+
+Congrats.propTypes = {
+    success: PropTypes.bool.isRequired,
+  };
+  
+  export default Congrats;
