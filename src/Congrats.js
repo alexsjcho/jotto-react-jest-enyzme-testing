b0abbdf09500414b6d6e9b8b6aac0d7f15ef530e
Alex Cho <alexsjcho@gmail.com>
2019-04-02 16:02:34 -0700

    Added Bootstrap classes to components

--- src/Congrats.js
+++ src/Congrats.js
@@ -7,11 +7,10 @@ import PropTypes from "prop-types";
  * @param {object} props - React props.
  * @returns {JSX.Element} - Rendered component (or null if `success` prop is false).
  */
-
 const Congrats = props => {
   if (props.success) {
     return (
-      <div data-test="component-congrats">
+      <div data-test="component-congrats" className="alert alert-success">
         <span data-test="congrats-message">
           Congratulations! You guessed the word!
         </span>
