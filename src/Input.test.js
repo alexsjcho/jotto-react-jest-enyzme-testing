d2cbd060fadc2c96addbe88e5f7b57a8b441fc2e
Alex Cho <alexsjcho@gmail.com>
2019-04-02 20:43:11 -0700

    updatedinput.test.js file

--- src/Input.test.js
+++ src/Input.test.js
@@ -5,7 +5,7 @@ import { findByTestAttr, storeFactory } from "../test/testUtils";
 import Input from "./Input";
 
 /**
- * Factory function to create a ShallowWrapper for the GuessedWords component.
+ * Factory function to create a ShallowWrapper for the Input component.
  * @function setup
  * @param {object} initialState - Initial state for this setup.
  * @returns {ShallowWrapper}
@@ -56,4 +56,17 @@ describe("render", () => {
     });
   });
 });
-describe("update state", () => {});
+
+describe("redux props", () => {
+  test("has success piece of state as prop", () => {
+    const success = true;
+    const wrapper = setup({ success });
+    const successProp = wrapper.instance().props.success;
+    expect(successProp).toBe(success);
+  });
+  test("`guessWord` action creator is a function prop", () => {
+    const wrapper = setup();
+    const guessWordProp = wrapper.instance().props.guessWord;
+    expect(guessWordProp).toBeInstanceOf(Function);
+  });
+});
